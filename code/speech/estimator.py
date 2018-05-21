5153b80798878c1190c960f5f7507289a9a5c709
github-user92 <j_johannsmeier@gmx.de>
2018-05-21 20:01:48 +0200

    added regularization

--- code/speech/estimator.py
+++ code/speech/estimator.py
@@ -9,11 +9,20 @@ N_CLASSES = 30
 
 
 def model_fn_linear(features, labels, mode, params):
-    logits = tf.layers.dense(features, N_CLASSES)
     base_lr = params["base_lr"]
     end_lr = params["end_lr"]
     decay_steps = params["decay_steps"]
     decay_power = params["decay_power"]
+    reg_type = params["reg_type"]
+    reg_coeff = params["reg_coeff"]
+    if reg_type == "l1":
+        reg = lambda x: tf.norm(x, ord=1)
+    elif reg_type == "l2":
+        reg = lambda x: tf.norm(x, ord=2)
+    else:
+        reg = None
+
+    logits = tf.layers.dense(features, N_CLASSES, kernel_regularizer=reg)
 
     if mode == tf.estimator.ModeKeys.PREDICT:
         predictions = {"logits": logits,
@@ -23,6 +32,13 @@ def model_fn_linear(features, labels, mode, params):
 
     cross_ent = tf.losses.sparse_softmax_cross_entropy(
         labels=labels, logits=logits)
+    tf.summary.scalar("cross_ent", cross_ent)
+    loss = cross_ent
+    reg_loss = tf.losses.get_regularization_loss()
+    tf.summary.scalar("reg_loss", reg_loss)
+    if reg_coeff:
+        loss += reg_coeff * reg_loss
+
     acc = tf.reduce_mean(
         tf.cast(tf.equal(tf.argmax(logits, axis=1, output_type=tf.int32), labels),
                 tf.float32))
@@ -33,12 +49,13 @@ def model_fn_linear(features, labels, mode, params):
         lr = tf.train.polynomial_decay(base_lr, gs, decay_steps, end_lr,
                                        decay_power)
         tf.summary.scalar("learing_rate", lr)
-        train_op = tf.train.AdamOptimizer(lr).minimize(cross_ent,
+        train_op = tf.train.AdamOptimizer(lr).minimize(loss,
                                                        global_step=gs)
         return tf.estimator.EstimatorSpec(mode=mode, loss=cross_ent,
                                           train_op=train_op)
 
-    eval_metric_ops = {"accuracy": tf.metrics.accuracy(labels, logits)}
+    eval_metric_ops = {"accuracy": tf.metrics.accuracy(labels, logits),
+                       "cross_ent": tf.metrics.mean(cross_ent)}
     return tf.estimator.EstimatorSpec(mode=mode, loss=cross_ent,
                                       eval_metric_ops=eval_metric_ops)
 
@@ -53,19 +70,25 @@ parser.add_argument("-B", "--batch_size",
 parser.add_argument("-L", "--learning_rate",
                     nargs=2,
                     type=float,
-                    default=[0.1, 0.000001],
+                    default=[0.001, 0.0000001],
                     help="Initial/final learning rate.")
 parser.add_argument("-D", "--decay",
                     nargs=2,
                     type=float,
                     default=[100000, 2.0],
                     help="Decay steps and power.")
+parser.add_argument("-R", "--reg",
+                    nargs=2,
+                    default=[None, 0.0],
+                    help="Regularization type and coefficient.")
 args = parser.parse_args()
 
 prms = {"base_lr": args.learning_rate[0],
         "end_lr": args.learning_rate[1],
         "decay_steps": int(args.decay[0]),
-        "decay_power": args.decay[1]}
+        "decay_power": args.decay[1],
+        "reg_type": args.reg[0],
+        "reg_coeff": float(args.reg[1])}
 
 tf.logging.set_verbosity(tf.logging.INFO)
 
